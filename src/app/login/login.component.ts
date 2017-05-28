cc69c589e763353cdfc336c30fb0465574d81a85
SAJED SHAIKH <SAJED SHAIKH>
2017-05-28 22:01:54 +0530

    implemented reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	public form: FormGroup;
 
-  OnSubmit(jsonForm) {
-  	console.log(jsonForm);
-  }
+	// form = new FormGroup({
+	// 	username: new FormControl("your username", [Validators.required]),
+	// 	password: new FormControl("your password", [Validators.required]),
+	// 	rememberme: new FormControl(true)
+	// });
+
+	constructor(private formBuilder: FormBuilder) { }
+
+	ngOnInit() {
+		this.form = this.formBuilder.group({
+			username: ['blah', Validators.required],
+			password: ['blah', Validators.required],
+			rememberme: [true]
+		});
+	}
+
+	OnSubmit(jsonForm) {
+		console.log(jsonForm);
+	}
 
 }
